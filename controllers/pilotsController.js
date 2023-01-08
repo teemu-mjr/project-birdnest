8b48c41c8d90f044bb66503ac53d0add45d47cf8
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-01-08 15:55:59 +0200

    feat(routes): add `/api/pilots/closest` to fetch the closest distance
    Add pisibility to fetch `ClosestDistance` from
    `reactorHelper`

--- controllers/pilotsController.js
+++ controllers/pilotsController.js
@@ -1,9 +1,14 @@
 const reactorHelper = require("../utils/reactorHelper");
 
-function getNaughty(_req, res) {
+const getNaughty = (_req, res) => {
   res.status(200).json(reactorHelper.getNaughtyPilots());
-}
+};
+const getClosest = (_req, res) => {
+  const meters = (reactorHelper.getClosestDistance() / 1000).toFixed(2);
+  res.status(200).json(meters);
+};
 
 module.exports = {
   getNaughty,
+  getClosest,
 };
--- routes/pilotsRouter.js
+++ routes/pilotsRouter.js
@@ -2,5 +2,6 @@ const pilotsRouter = require("express").Router();
 const pilotsController = require("../controllers/pilotsController");
 
 pilotsRouter.get("/", pilotsController.getNaughty);
+pilotsRouter.get("/closest", pilotsController.getClosest);
 
 module.exports = pilotsRouter;
